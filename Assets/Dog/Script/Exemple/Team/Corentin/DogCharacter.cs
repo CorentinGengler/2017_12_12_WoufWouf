bc9f009f57d63c422268da19facc2865181a2dbc
CorentinGengler <gengcor@gmail.com>
2017-12-12 16:55:17 +0100

    DogCharacterYay
    he barks !!! he bbbaaaarrrrkkkkss!!

---
+++ Assets/Dog/Script/Exemple/Team/Corentin/DogCharacter.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogCharacter  : Dog
+{
+
+    #region Public Members
+    public Rigidbody _Rigidbody;
+    public float _force;
+    public AudioSource _DogSound;
+    #endregion
+    
+
+    #region Public Void
+    public override void Bark(float volume, float frequence)
+    {
+        _DogSound.volume = volume;
+        _DogSound.Play();
+
+    }
+
+
+    public override void Jump(float heighCm)
+    {
+        _Rigidbody.AddForce(Vector3.up * heighCm * _force, ForceMode.Impulse);
+    }
+    #endregion
+
+
+    #region System
+
+    void Start () 
+    {
+		
+	}
+    void Awake () 
+    {
+		
+	}
+	
+	void Update () 
+    {
+		
+	}
+
+    #endregion
+
+    #region Private Void
+
+    #endregion
+
+    #region Tools Debug And Utility
+
+    #endregion
+
+
+    #region Private And Protected Members
+
+    #endregion
+
+}
---
+++ Assets/Dog/Script/Exemple/Team/Corentin/TDD_DogCharacter.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TDD_DogCharacter  : MonoBehaviour
+{
+
+    #region Public Members
+
+    [SerializeField]
+    private Dog _dog;
+    private IDog _dogInterface;
+
+    [SerializeField]
+    private float _timeBetweenJumpMin = 0.1f;
+    [SerializeField]
+    private float _timeBetweenJumpMax = 5f;
+
+    [SerializeField]
+    private float _timeBetweenBarkMin = 0.5f;
+    [SerializeField]
+    private float _timeBetweenBarkMax = 3f;
+    #endregion
+
+
+    #region Public Void
+
+    #endregion
+
+
+    #region System
+
+    void Start () 
+    {
+        StartCoroutine(Bark());
+        StartCoroutine(Jump());
+    }
+
+    private IEnumerator Bark()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(
+               UnityEngine.Random.Range(_timeBetweenBarkMin, _timeBetweenBarkMax));
+            _dog.Bark(UnityEngine.Random.Range(-0.5f, 2f), UnityEngine.Random.Range(0, 1000));
+            
+
+        }
+
+    }
+    private IEnumerator Jump()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(
+                UnityEngine.Random.Range(_timeBetweenJumpMin, _timeBetweenJumpMax));
+            _dog.Jump(UnityEngine.Random.Range(100, 200));
+        }
+
+    }
+    void Awake () 
+    {
+		
+	}
+	
+	void Update () 
+    {
+		
+	}
+
+    #endregion
+
+    #region Private Void
+
+    #endregion
+
+    #region Tools Debug And Utility
+
+    #endregion
+
+
+    #region Private And Protected Members
+
+    #endregion
+
+}
